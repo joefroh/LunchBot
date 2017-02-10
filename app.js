8ef54d7a0d32b6feb749ed9e0f50fd797e1e4a68
Joey Frohlinger <jmafrohlinger@outlook.com>
2017-02-09 16:14:34 -0800

    Starting to flesh out profile population logic

--- app.js
+++ app.js
@@ -23,6 +23,46 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World, I changed it");
-});
+bot.dialog('/', [
+    function (session, args, next) {
+        if(session.message.text == '/delete'){
+            session.userData.name = null;
+        }
+       if(!session.userData.name){
+           session.beginDialog('/profile');
+       }
+       else{
+           next();
+       }
+    },
+    function (session) {
+        session.send('Hey %s! What can I do for you?', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile',[
+    function (session, args){
+        if(args == 'retry'){
+            builder.Prompts.text(session,"What is your paypal.me id?");
+        }
+        else{
+            builder.Prompts.text(session, "Hey! This is your first time chatting with me, we need to set up your profile.\r\nWhat is your paypal.me name?");
+        }
+    },
+    function(session, results){
+        session.userData.name = results.response;
+        
+        builder.Prompts.confirm(session, "Is "+ session.userData.name + " correct?");
+    },
+    function(session, results){
+        if(results.response == true)
+        {
+            session.endDialog();
+        }
+        else
+        {
+            session.send("Good thing we caught that! Let's try again")
+            session.beginDialog('/profile','retry');
+        }
+    }
+]);
